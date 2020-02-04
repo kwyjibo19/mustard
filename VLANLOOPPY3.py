a81b505a89bd0847aa6b0c642060f6e5e19b1411
Marlon Sotta <marlonsotta@gmail.com>
2020-02-04 17:17:43 +0000

    add vlan loop

---
+++ VLANLOOPPY3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import getpass
+import telnetlib
+
+HOST = "99.99.99.3"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"!HELLO WORLD!\n")
+tn.write(b"conf t\n")
+
+for n in range (90,101):
+	tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+	tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
